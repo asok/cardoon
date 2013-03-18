b75c98f5a74b506978fcbb87522962b07bf187ad
asok <adam.sokolnicki@gmail.com>
2013-03-18 14:56:19 +0100

    Compare with RSpec version 2.13.0

--- lib/cardoon.rb
+++ lib/cardoon.rb
@@ -3,7 +3,7 @@ require 'cardoon/steps_methods'
 
 RSpec.configuration.include Cardoon::StepsMethods, :capybara_feature => true
 
-if Gem::Version.new(RSpec::Core::Version::STRING) <= Gem::Version.new("2.12.0")
+if Gem::Version.new(RSpec::Core::Version::STRING) <= Gem::Version.new("2.13.0")
   class Cardoon::Steps
     def success_color(text)
       green(text)
